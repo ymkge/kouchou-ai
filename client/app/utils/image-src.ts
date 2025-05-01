b4a21b546c6fb9971d2f11bda8dd2e7789332b05
mtane0412 <mtane0412@gmail.com>
2025-05-01 22:43:12 +0900

    getImageFromServerSrcでstatic export時にbasePath設定を反映する

--- client/app/utils/image-src.ts
+++ client/app/utils/image-src.ts
@@ -1,6 +1,7 @@
 export const getImageFromServerSrc = (src: string) => {
   if (process.env.NEXT_PUBLIC_OUTPUT_MODE === "export" && src) {
-    return src;
+    const staticExportBasePath = process.env.NEXT_PUBLIC_STATIC_EXPORT_BASE_PATH || "";
+    return staticExportBasePath + src;
   }
 
   const basePath = process.env.NEXT_PUBLIC_API_BASEPATH;
