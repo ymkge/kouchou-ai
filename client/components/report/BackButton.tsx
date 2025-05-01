9bf63149987cbc4ac62a09042e8e92f14c51817a
mtane0412 <mtane0412@gmail.com>
2025-05-01 22:52:40 +0900

    一覧に戻るからbasePathを除去して戻れるようにした

--- client/components/report/BackButton.tsx
+++ client/components/report/BackButton.tsx
@@ -1,16 +1,12 @@
-import getConfig from "next/config";
 import Link from "next/link";
 
 import { Box, Button } from "@chakra-ui/react";
 import { ChevronLeft } from "lucide-react";
 
 export function BackButton() {
-  const { publicRuntimeConfig } = getConfig() || { publicRuntimeConfig: {} };
-  const basePath = publicRuntimeConfig.basePath || "";
-
   return (
     <Box w={"fit-content"} mx={"auto"}>
-      <Link href={`${basePath}/`}>
+      <Link href={"/"}>
         <Button variant={"outline"} size={"md"}>
           <ChevronLeft />
           一覧へ戻る
