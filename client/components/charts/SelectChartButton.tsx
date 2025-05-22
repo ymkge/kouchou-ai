df993631d6748724de21b549f7309effe22766ae
tokoroten <shinta.nakayama@gmail.com>
2025-05-22 14:25:31 +0900

    高さを修正

--- client/components/charts/SelectChartButton.tsx
+++ client/components/charts/SelectChartButton.tsx
@@ -89,7 +89,7 @@ export function SelectChartButton({
           h={["80px", null, "56px"]}
         >
           <SegmentGroup.Indicator bg="white" border="1px solid #E4E4E7" boxShadow="0 4px 6px 0 rgba(0, 0, 0, 0.1)" />
-          <SegmentGroup.Items items={items} w={["calc(100% / 3)", null, "162px"]} />
+          <SegmentGroup.Items items={items} w={["calc(100% / 3)", null, "162px"]} h="56px" />
         </SegmentGroup.Root>
 
         <HStack gap={1} justifySelf={["end"]} alignSelf={"center"}>
