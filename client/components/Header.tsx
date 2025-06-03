03ea1dcaaa2650a037002e04c010fa861a04e1dd
shgtkshruch <shgtk.shruch@gmail.com>
2025-06-03 17:05:29 +0900

    Change global layout

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -5,7 +5,7 @@ import { HStack, Image } from "@chakra-ui/react";
 
 export function Header() {
   return (
-    <HStack justify="space-between" mb={8} mx={"auto"} maxW={"1200px"}>
+    <HStack justify="space-between" py="5" mb={8} mx={"auto"} maxW={"1200px"}>
       <Image src="/images/dd2030-logo.svg" alt="デジタル民主主義2030" w="180px" h="60px" />
       <BroadlisteningGuide />
     </HStack>
