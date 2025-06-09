873cf5e8ce0e26ff74564efc1587bb7ba6c8d2aa
shgtkshruch <shgtk.shruch@gmail.com>
2025-06-09 19:27:56 +0900

    Add textStyle

---
+++ client/components/theme/textStyle.ts
@@ -0,0 +1,79 @@
+import { defineTextStyles } from "@chakra-ui/react"
+
+export const textStyles = defineTextStyles({
+  'heading/3xl': {
+    value: {
+      fontSize: "3xl",
+      fontWeight: "bold",
+      lineHeight: "tall",
+      letterSpacing: "0.9px",
+    },
+  },
+  'heading/2xl': {
+    value: {
+      fontSize: "2xl",
+      fontWeight: "bold",
+      lineHeight: "tall",
+      letterSpacing: "0.75px",
+    },
+  },
+  'heading/xl': {
+    value: {
+      fontSize: "xl",
+      fontWeight: "bold",
+      lineHeight: "tall",
+      letterSpacing: "0.6px",
+    },
+  },
+  'body/lg': {
+    value: {
+      fontSize: "lg",
+      fontWeight: "normal",
+      lineHeight: "taller",
+      letterSpacing: "0.5px",
+    },
+  },
+  'body/md': {
+    value: {
+      fontSize: "md",
+      fontWeight: "normal",
+      lineHeight: "taller",
+      letterSpacing: "0.4px",
+    },
+  },
+  'body/sm': {
+    value: {
+      fontSize: "xs",
+      fontWeight: "normal",
+      lineHeight: "taller",
+      letterSpacing: "0.3px",
+    },
+  },
+  'body/lg/bold': {
+    value: {
+      fontFamily: "body",
+      fontSize: "lg",
+      fontWeight: "bold",
+      lineHeight: "taller",
+      letterSpacing: "0.5px",
+    },
+  },
+  'body/md/bold': {
+    value: {
+      fontFamily: "body",
+      fontSize: "md",
+      fontWeight: "bold",
+      lineHeight: "taller",
+      letterSpacing: "0.4px",
+    },
+  },
+  'body/sm/bold': {
+    value: {
+      fontFamily: "body",
+      fontSize: "xs",
+      fontWeight: "bold",
+      lineHeight: "taller",
+      letterSpacing: "0.3px",
+    },
+  },
+})
