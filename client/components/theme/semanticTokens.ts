6a9e9e6f88294c2888a33b22cc3efef6e9d90d61
shgtkshruch <shgtk.shruch@gmail.com>
2025-06-11 15:45:45 +0900

    Add semantic tokens

---
+++ client/components/theme/semanticTokens.ts
@@ -0,0 +1,14 @@
+export const semanticTokens = {
+  colors: {
+    font: {
+      primary: { value: "{colors.gray.800}" },
+      secondary: { value: "{colors.gray.500}" },
+      disable: { value: "{colors.gray.400}" },
+      white: { value: "{colors.white}" },
+      link: { value: "{colors.blue.500}" },
+      visited: { value: "{colors.purple.700}" },
+      error: { value: "{colors.error.500}" },
+      aiTalking: { value: "{colors.purple.600}" },
+    },
+  },
+};
