0605e5d22f7bdd9265af854da2a8d61b7ae5ae8e
shgtkshruch <shgtk.shruch@gmail.com>
2025-06-23 21:00:55 +0900

    Fix jest setting

--- client-admin/jest.setup.js
+++ client-admin/jest.setup.js
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+
+if(!global.structuredClone){
+  global.structuredClone = function structuredClone(objectToClone) {
+    if (objectToClone === undefined) return undefined;
+    return JSON.parse(JSON.stringify(objectToClone));
+  };
+}
