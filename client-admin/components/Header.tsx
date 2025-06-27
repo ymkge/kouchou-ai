23e06d18028c1c33d4e928bb618c97825ce3a298
shgtkshruch <shgtk.shruch@gmail.com>
2025-06-27 18:53:15 +0900

    Change header design

--- client-admin/components/Header.tsx
+++ client-admin/components/Header.tsx
@@ -1,44 +1,20 @@
-import type { Meta } from "@/type";
-import { Alert, HStack, Image } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Alert, Box, Flex, Image } from "@chakra-ui/react";
 
 export function Header() {
-  const [meta, setMeta] = useState<Meta | null>(null);
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  // メタ情報の取得
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_BASEPATH}/meta`)
-      .then((response) => response.json())
-      .then((data) => setMeta(data))
-      .catch(() => setMeta(null));
-  }, []);
-
   return (
-    <HStack justify="space-between" alignItems={"center"} mb={8} mx={"auto"} maxW={"1200px"}>
-      <HStack>
-        {meta && !meta.isDefault && (
-          <Image
-            src={`${process.env.NEXT_PUBLIC_API_BASEPATH}/meta/reporter.png`}
-            mx={"auto"}
-            objectFit={"cover"}
-            maxH={{ base: "40px", md: "60px" }}
-            maxW={{ base: "120px", md: "200px" }}
-            alt={meta.reporter}
-            onLoad={() => setImageLoaded(true)}
-            display={imageLoaded ? "block" : "none"}
-          />
-        )}
-      </HStack>
-      <HStack>
-        <Alert.Root status="warning">
-          <Alert.Indicator />
-          <Alert.Content>
-            <Alert.Title fontSize={"md"}>管理者画面</Alert.Title>
-            <Alert.Description>このページはレポート作成者向けの管理画面です</Alert.Description>
-          </Alert.Content>
-        </Alert.Root>
-      </HStack>
-    </HStack>
+    <Box py="5" px="5" bg="white">
+      <Flex maxW="1200px" mx="auto" justifyContent="space-between" alignItems="center">
+        <Image src="/images/dd2030-logo.svg" alt="デジタル民主主義2030" width="180px" height="60px" />
+        <Box>
+          <Alert.Root status="warning">
+            <Alert.Indicator />
+            <Alert.Content>
+              <Alert.Title fontSize={"md"}>管理者画面</Alert.Title>
+              <Alert.Description>このページはレポート作成者向けの管理画面です</Alert.Description>
+            </Alert.Content>
+          </Alert.Root>
+        </Box>
+      </Flex>
+    </Box>
   );
 }
