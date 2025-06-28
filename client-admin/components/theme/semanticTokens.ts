bf583c742b8cb1bbe6e500cdd20f77a19bbdb5b9
shgtkshruch <shgtk.shruch@gmail.com>
2025-06-28 19:06:12 +0900

    Fix token name

--- client-admin/components/theme/semanticTokens.ts
+++ client-admin/components/theme/semanticTokens.ts
@@ -7,7 +7,7 @@ export const semanticTokens = {
       white: { value: "{colors.white}" },
       link: { value: "{colors.blue.500}" },
       visited: { value: "{colors.purple.700}" },
-      error: { value: "{colors.error.500}" },
+      error: { value: "{colors.red.500}" },
       aiTalking: { value: "{colors.purple.600}" },
     },
     button: {
