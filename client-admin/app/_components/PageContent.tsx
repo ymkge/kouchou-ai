86fd69195114ffd02537d1deade1cf5b3f2b5807
shgtkshruch <shgtk.shruch@gmail.com>
2025-07-11 22:33:39 +0900

    Add visibility count ui

--- client-admin/app/_components/PageContent.tsx
+++ client-admin/app/_components/PageContent.tsx
@@ -1,5 +1,6 @@
 import type { Report } from "@/type";
-import { Button, HStack, Heading, Link } from "@chakra-ui/react";
+import { Button, Flex, HStack, Heading, Icon, Link, Text, VStack } from "@chakra-ui/react";
+import { Eye, EyeClosedIcon, LockKeyhole } from "lucide-react";
 import { BuildDownloadButton } from "./BuildDownloadButton/BuildDownloadButton";
 import { Empty } from "./Empty";
 import { ReportCardList } from "./ReportCardList";
@@ -11,9 +12,35 @@ type Props = {
 export function PageContent({ reports }: Props) {
   return (
     <>
-      <Heading textStyle="heading/xl">
-        レポート管理
-      </Heading>
+      <Flex justifyContent="space-between">
+        <Heading textStyle="heading/xl">レポート管理</Heading>
+        <Flex gap="3">
+          <VStack gap="2" w="88px" h="80px" bg="white" justifyContent="center">
+            <Icon color="font.public">
+              <Eye />
+            </Icon>
+            <Text textStyle="body/lg/bold" lineHeight="1.38">
+              0
+            </Text>
+          </VStack>
+          <VStack gap="2" w="88px" h="80px" bg="white" justifyContent="center">
+            <Icon color="font.limitedPublic">
+              <LockKeyhole />
+            </Icon>
+            <Text textStyle="body/lg/bold" lineHeight="1.38">
+              0
+            </Text>
+          </VStack>
+          <VStack gap="2" w="88px" h="80px" bg="white" justifyContent="center">
+            <Icon color="font.error">
+              <EyeClosedIcon />
+            </Icon>
+            <Text textStyle="body/lg/bold" lineHeight="1.38">
+              0
+            </Text>
+          </VStack>
+        </Flex>
+      </Flex>
       {reports.length === 0 ? (
         <Empty />
       ) : (
