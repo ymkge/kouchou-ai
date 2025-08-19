873a716331741cc3006b8a808691499d6c09c627
mochizuki-pg <t.mochizuki.pg@gmail.com>
2025-08-19 22:43:17 +0900

    feat: Update header logo and add new SVG logo file

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -6,7 +6,7 @@ import { HStack, Image } from "@chakra-ui/react";
 export function Header() {
   return (
     <HStack justify="space-between" py="5" mb={8} mx={"auto"} maxW={"1200px"}>
-      <Image src="/images/dd2030-logo.svg" alt="デジタル民主主義2030" w="180px" h="60px" />
+      <Image src="/images/logo.svg" alt="広聴AI" />
       <BroadlisteningGuide />
     </HStack>
   );
