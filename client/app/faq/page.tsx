fc46ef11159ffba8c809936aaea81fd67b3f80f6
shgtkshruch <shgtk.shruch@gmail.com>
2025-09-17 19:18:09 +0900

    Add error handling

--- client/app/faq/page.tsx
+++ client/app/faq/page.tsx
@@ -7,21 +7,31 @@ import { Contact } from "./Contact";
 import { Faq } from "./Faq";
 
 export default async function Page() {
-  const metaResponse = await fetch(`${getApiBaseUrl()}/meta/metadata.json`, {
-    next: { tags: ["meta"] },
-  });
-  const meta: Meta = await metaResponse.json();
+  try {
+    const metaResponse = await fetch(`${getApiBaseUrl()}/meta/metadata.json`, {
+      next: { tags: ["meta"] },
+    });
+    const meta: Meta = await metaResponse.json();
 
-  return (
-    <>
-      <Header />
-      <Box className="container" bg="#EFF6FF" pb="24">
-        <Box mx="auto" maxW="1024px">
-          <Faq />
-          <Contact />
+    return (
+      <>
+        <Header />
+        <Box className="container" bg="#EFF6FF" pb="24">
+          <Box mx="auto" maxW="1024px">
+            <Faq />
+            <Contact />
+          </Box>
         </Box>
-      </Box>
-      <Footer meta={meta} />
-    </>
-  );
+        <Footer meta={meta} />
+      </>
+    );
+  } catch (e) {
+    return (
+      <p>
+        エラー：データの取得に失敗しました
+        <br />
+        Error: fetch failed to {process.env.NEXT_PUBLIC_API_BASEPATH}.
+      </p>
+    );
+  }
 }
